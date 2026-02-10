ee8a96d60136ee05c1327d519ecc5ada70235e76
Adelua Victor <adeluav3@gmail.com>
2026-02-10 14:49:12 +0100

    SEO v2

--- src/components/seo/ServicesHubPage.jsx
+++ src/components/seo/ServicesHubPage.jsx
@@ -1,6 +1,5 @@
 // src/components/seo/ServicesHubPage.jsx - MOBILE OPTIMIZED
 import React from 'react';
-import { Helmet } from 'react-helmet-async';
 import { Link } from 'react-router-dom';
 import SimpleHelmet from './SimpleHelmet';
 
